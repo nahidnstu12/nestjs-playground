8b8fb1dfb196e3e8630f3a3a18c70db75dfe2d35
nahid_dev <mazharul.islam.nahid@softbdltd.com>
2023-05-19 09:56:02 +0600

    database config setup

--- with-sequlize/src/app.module.ts
+++ with-sequlize/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { AppConfigModule } from './shared/app-config/app-config.module';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AppConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
