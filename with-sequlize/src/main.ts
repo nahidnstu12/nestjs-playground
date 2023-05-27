3a6b156f43757485d0e3e418e7af5fe9c7168caa
nahidnstu12 <nahid.dev19@gmail.com>
2023-05-27 09:53:40 +0600

    doesuser exists & validationInput pipe implement

--- with-sequlize/src/main.ts
+++ with-sequlize/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000, ()=> console.log("Application starts 5000"));
+  app.useGlobalPipes(new ValidateInputPipe());
+  await app.listen(5000, () => console.log('Application starts 5000'));
 }
 bootstrap();
